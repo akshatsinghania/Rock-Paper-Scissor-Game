2d14c39893c679a78f88d88dabee31923a898e8c
akshatsinghania <singhaniaakshat1@gmail.com>
2024-07-24 23:49:02 +0530

    looped through all images and styled the app

---
+++ script.js
@@ -0,0 +1,20 @@
+const gameContainer = document.querySelector(".container"),
+  userResult = document.querySelector(".user_result img"),
+  cpuResult = document.querySelector(".cpu_result img"),
+  result = document.querySelector(".result"),
+  optionImages = document.querySelectorAll(".option_image");
+
+//Loop through each option image element
+optionImages.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    image.classList.add("active");
+
+    optionImages.forEach((image2, index2) => {
+      index !== index2 && image2.classList.remove("active");
+
+      let imageSrc = e.target.querySelector("img").src;
+      userResult.src = imageSrc;
+      console.log(imageSrc);
+    });
+  });
+});
